f2b37e668981d35c8058d9d485c278c70b3fc125
Ryan Lee <djrlee51@gmail.com>
2017-05-09 10:38:22 -0700

    finished coming soon pages and ready to go live

--- src/client/components/Sides.js
+++ src/client/components/Sides.js
@@ -1,57 +1,64 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Side from 'assets/images/sideCopy.png'
+import 'assets/styles/dessert.css'
 
 export default React.createClass({
   render: function (){
     return(
       <div id="sidesContainer">
-        <div className="sidesRecipeContainer">
-          <div className="sidesPhoto">
-            <img className="sidesImage" src={sides1}/>
-          </div>
-          <div className="sidesSummary">
-            <h3 className="sidesTitle"></h3>
-            <p className="sidesRecipeSummary"></p>
-          </div>
-          <h3 className="serveAmount">Serves 4</h3>
-          <div className="sidesIngredients">
-            <div className="sidesIngredient">
-              <h3 className="sidesIngredientTitle"></h3>
-              <p className="sidesEachIngredient"></p>
-            </div>
-          </div>
-          <p className="ovenTemp">Pre heat the oven to 375 degrees</p>
-          <div className="sidesDirections">
-            <div className="sidesDirection">
-              <h3 className="sidesDirectionTitle"></h3>
-              <p className="sidesDirectionSummary"></p>
-            </div>
-          </div>
-        </div>
-        <div className="sidesRecipeContainer2">
-          <div className="sidesPhoto">
-            <img className="sidesImage" src={sides2}/>
-          </div>
-          <div className="sidesSummary">
-            <h3 className="sidesTitle"></h3>
-            <p className="sidesRecipeSummary"></p>
-          </div>
-          <h3 className="serveAmount">Serves 4</h3>
-          <div className="sidesIngredients2">
-            <div className="sidesIngredient">
-              <h3 className="sidesIngredientTitle"></h3>
-              <p className="sidesEachIngredient"></p>
-            </div>
-          </div>
-          <p className="ovenTemp">Pre heat the oven to 375 degrees</p>
-          <div className="sidesDirections">
-            <div className="sidesDirection">
-              <h3 className="sidesDirectionTitle"></h3>
-              <p className="sidesDirectionSummary"></p>
-            </div>
-          </div>
+        <div className="comingContainer">
+          <img className="comingSoon" src={Side}/>
         </div>
       </div>
     )
   }
 })
+
+
+// <div className="sidesRecipeContainer">
+//           <div className="sidesPhoto">
+//             <img className="sidesImage" src={sides1}/>
+//           </div>
+//           <div className="sidesSummary">
+//             <h3 className="sidesTitle"></h3>
+//             <p className="sidesRecipeSummary"></p>
+//           </div>
+//           <h3 className="serveAmount">Serves 4</h3>
+//           <div className="sidesIngredients">
+//             <div className="sidesIngredient">
+//               <h3 className="sidesIngredientTitle"></h3>
+//               <p className="sidesEachIngredient"></p>
+//             </div>
+//           </div>
+//           <p className="ovenTemp">Pre heat the oven to 375 degrees</p>
+//           <div className="sidesDirections">
+//             <div className="sidesDirection">
+//               <h3 className="sidesDirectionTitle"></h3>
+//               <p className="sidesDirectionSummary"></p>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="sidesRecipeContainer2">
+//           <div className="sidesPhoto">
+//             <img className="sidesImage" src={sides2}/>
+//           </div>
+//           <div className="sidesSummary">
+//             <h3 className="sidesTitle"></h3>
+//             <p className="sidesRecipeSummary"></p>
+//           </div>
+//           <h3 className="serveAmount">Serves 4</h3>
+//           <div className="sidesIngredients2">
+//             <div className="sidesIngredient">
+//               <h3 className="sidesIngredientTitle"></h3>
+//               <p className="sidesEachIngredient"></p>
+//             </div>
+//           </div>
+//           <p className="ovenTemp">Pre heat the oven to 375 degrees</p>
+//           <div className="sidesDirections">
+//             <div className="sidesDirection">
+//               <h3 className="sidesDirectionTitle"></h3>
+//               <p className="sidesDirectionSummary"></p>
+//             </div>
+//           </div>
+//         </div>
